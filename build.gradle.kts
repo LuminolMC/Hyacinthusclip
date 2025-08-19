092762f02f80b0c769786fb07b6a948764c0b1b9
xiaoxijun <A3167717663@hotmail.com>
2025-08-20 03:32:10 +0800

    update maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,7 +115,7 @@ publishing {
         }
 
         repositories {
-            maven("https://repo.menthamc.com/repository/maven-snapshots/") {
+            maven("https://repo.menthamc.org/repository/maven-snapshots/") {
                 name = "MenthaMC"
                 credentials(PasswordCredentials::class) {
                     username = System.getenv("PRIVATE_MAVEN_REPO_USERNAME")
